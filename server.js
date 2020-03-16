fc3511c208c15165dde89b5878a99866e3d31578
Mohamed <mohamedennoun@gmail.com>
2020-03-16 15:41:17 +0100

    Exercice 4, 5 end

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 // Parsing
 app.use(express.json())
@@ -8,6 +9,53 @@ app.use(express.json())
 app.get('/', function (req, res) {	
 	res.send('Hello World')
 })
+
+// POST method route
+
+app.get('/hello', function(req, res) {
+
+  
+  var nom = req.query.nom
+  if(!req.query.nom){
+    res.send("Quel est votre nom ?")
+
+  } else {
+    res.send("Bonjour " + nom )
+  }
+
+})
+
+// POST method route
+app.post('/chat', function (req, res) {		
+
+	var message = req.body.msg.split(' ')
+	var k = message[0]
+
+	// Read file
+	var content = fs.readFileSync('reponses.json')
+	var object = JSON.parse(content)
+
+	if (message.length === 1) {
+
+		(k in object) ? 
+		res.send(k + ': ' + object[k])
+		: res.send('Je ne connais pas ' + k + '...')
+	}
+
+	if (message.length === 3) {
+
+		object[k] = message[2]
+
+		// Write in file
+		var data = JSON.stringify(object)
+		fs.writeFileSync('reponses.json', data)
+
+		res.send('Merci pour cette information !')
+	}
+})
+
+
+
 
 var port = process.env.PORT || 3000;
 
